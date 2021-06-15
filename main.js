785c4a8bb6639a6426137acd51bd9a238cdcc3d9
blade-01 <animashauntaofiq@yahoo.com>
2021-06-15 04:58:23 +0100

    Added basic functionality

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-let inputValue = "Nigeria";
-let key = "661984f378bc7b8bb29169ab2d7c6b6e";
+// Variables
+const form = document.querySelector("form");
+const input = document.querySelector("#weather");
 
-function getData() {
+// Event Listener
+form.addEventListener("submit", fetchWeather);
+
+// Fetch Function
+function fetchWeather(e) {
+  e.preventDefault();
+  const key = "661984f378bc7b8bb29169ab2d7c6b6e";
+  const inputValue = input.value;
   fetch(
-    "api.openweathermap.org/data/2.5/weather?q=London&appid=661984f378bc7b8bb29169ab2d7c6b6e&units=metric"
+    `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${key}&units=metric`
   )
-    .then((res) => res.json())
+    .then((res) => {
+      if (res.status !== 200) {
+        // Show Output
+        throw Error(res.statusText);
+      } else {
+        return res.json();
+      }
+    })
     .then((data) => {
-      console.log(data);
+      const { main, name, sys, weather } = data;
+      console.log(Math.round(main.temp));
+      console.log(name, sys.country);
+      console.log(weather[0].main);
     })
     .catch((err) => console.log(err));
+  document.querySelector("#weather").value = "";
 }
-getData();
