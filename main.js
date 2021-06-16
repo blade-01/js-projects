dc2c15710bb1beab42ec6f9a536ceeefda5af33d
blade-01 <animashauntaofiq@yahoo.com>
2021-06-16 22:48:17 +0100

    Style: Styled all components
    This was done to make the page responsuve

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 // Variables
 const form = document.querySelector("form");
 const input = document.querySelector("#weather");
+const timeStamp = document.querySelector(".time");
+const dayStamp = document.querySelector(".day");
+const body = document.querySelector("body");
+const temp = document.querySelector(".temp-details h2");
+const greet = document.querySelector(".temp-details p");
 
 // Event Listener
 form.addEventListener("submit", fetchProfile);
@@ -15,7 +20,8 @@ function fetchProfile(e) {
   )
     .then((res) => {
       if (res.status !== 200) {
-        // Show Output
+        // Show Error Alert
+        showAlert("Please provide a valid location", "error");
         throw Error(res.statusText);
       } else {
         return res.json();
@@ -30,3 +36,69 @@ function fetchProfile(e) {
     .catch((err) => console.log(err));
   document.querySelector("#weather").value = "";
 }
+
+function showAlert(message, alert) {
+  const div = document.createElement("div");
+  div.setAttribute("class", `alert ${alert}`);
+  div.appendChild(document.createTextNode(message));
+  const container = document.querySelector(".container");
+  const form = document.querySelector("form");
+  container.insertBefore(div, form);
+  setTimeout(() => {
+    document.querySelector(".alert").remove();
+  }, 2000);
+}
+
+// Time
+function showTime() {
+  let time = new Date(),
+    day = time.getDay(),
+    days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ],
+    hour = time.getHours(),
+    mins = time.getMinutes();
+  // Set AM PM
+  const amPm = hour >= 12 ? "PM" : "AM";
+  //  12hr Format
+  hour = hour % 12 || 12;
+  // Add to HTML
+  timeStamp.textContent = `${hour}:${addZero(mins)} ${amPm}`;
+  dayStamp.textContent = `${days[day]}`;
+
+  setTimeout(showTime, 1000);
+}
+
+// Add Zero to minutes
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+// Add Daytime or Nighttime
+function setByGreet() {
+  const time = new Date(),
+    hour = time.getHours();
+  if (hour < 12) {
+    body.style.cssText =
+      "background: url('/img/morning.jpg') no-repeat center center/cover; color: rgb(0, 9, 27);";
+    greet.textContent = "GOOD MORNING";
+  } else if (hour < 18) {
+    body.style.cssText =
+      "background: url('/img/afternoon.jpg') center center/cover;";
+    greet.textContent = "GOOD AFTERNOON";
+  } else {
+    body.style.cssText =
+      "background: url('/img/evening.jpg') center center/cover;";
+    greet.textContent = "GOOD EVENING";
+  }
+}
+
+// Call
+showTime();
+setByGreet();
