99a942f4ea0d85ad8ee7c2b0d3bf73c3588c0f51
blade-01 <animashauntaofiq@yahoo.com>
2021-06-18 04:48:43 +0100

    Feat: Added the search feature
    This enables user to search through pictures from the app

--- main.js
+++ main.js
@@ -4,13 +4,15 @@ const input = document.querySelector("#search");
 
 // Event Listener
 form.addEventListener("submit", fetchPhoto);
-fetchPhoto();
+// fetchPhoto();
 // Fetch Function
 function fetchPhoto(e) {
   e.preventDefault();
   const key = "4PADPOVyni-pXD_JEn8Hk36ENMebemzIz7ys9k2fH8U";
   const photo = input.value;
-  fetch(`https://api.unsplash.com/${photo}/photos?client_id=${key}`)
+  fetch(
+    `https://api.unsplash.com/search/photos?query=${photo}&client_id=${key}&per_page=6`
+  )
     .then((res) => {
       if (res.status !== 200) {
         // Show Output
@@ -20,8 +22,28 @@ function fetchPhoto(e) {
       }
     })
     .then((data) => {
-      // const { main, name, sys, weather } = data;
-      console.log(data);
+      // const {
+      //   urls: { raw },
+      //   user: { first_name },
+      //   location,
+      // } = data;
+      // let output = "";
+      // for (results in data){
+
+      // }
+      const results = data.results;
+      let output = "";
+      results.forEach((result) => {
+        output += `<div class="card placeholder-item">
+                    <img src="${result.urls.small}" loading="lazy" alt="img" />
+                    <div class="card-body">
+                      <h1>${result.user.first_name}</h1>
+                      <p>${result.user.location}</p>
+                    </div>
+                  </div>`;
+      });
+      document.querySelector(".cards").innerHTML = output;
+      console.log(results);
     })
     .catch((err) => console.log(err));
   document.querySelector("#search").value = "";
