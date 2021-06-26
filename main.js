bf5a048b1ed28fcecb69a7cd508167591663eda2
blade-01 <animashauntaofiq@yahoo.com>
2021-06-26 03:52:28 +0100

    Refactor: Added styles, functionalities
    Added styles for responsiveness and search functionality for user-experience

--- main.js
+++ main.js
@@ -1,4 +1,16 @@
-function fetchMovie() {
+// Variables
+const input = document.querySelector("#search");
+const form = document.querySelector("form");
+const select = document.querySelector(".select");
+const selectOptions = document.querySelectorAll(".selectdrop li");
+const descriptions = document.querySelectorAll(".desc");
+
+// Submit Event
+form.addEventListener("submit", fetchMovie);
+
+// Fetch Data
+function fetchMovie(e) {
+  e.preventDefault();
   let key = "683fa3d8c5f2bf2d5c60a5256d795c04";
   let movie = input.value;
   fetch(
@@ -15,16 +27,42 @@ function fetchMovie() {
     .then((data) => {
       let output = "";
       data.results.forEach((result) => {
-        console.log(result);
-        const backdrop = document.querySelector(".backdrop");
-        const poster = document.querySelector(".poster");
-        output += `<h1>${result.name}</h1>
-                  <img src="${result.poster_path}" alt="img">`;
+        output += `<div class="card">
+                    <div class="card-img">
+                      <img
+                        src="https://image.tmdb.org/t/p/w300${
+                          result.poster_path ||
+                          result.profile_path ||
+                          result.backdrop_path ||
+                          `https://image.tmdb.org/t/p/w500/wwemzKWzjKYJFfCeiB57q3r4Bcm.png`
+                        }"
+                        alt="country-img"
+                        loading="lazy"
+                        class="img"
+                      />
+                    </div>
+                    <div class="card-body">
+                      <h4>${
+                        result.original_title || result.name || result.title
+                      }</h4>
+                      <p>${
+                        result.overview
+                          ? result.overview
+                          : "No description available"
+                      }</p>
+                      <p><b>Release Date:</b> ${
+                        result.release_date ? result.release_date : "TBD"
+                      }</p>
+                      <div class="media">
+                        <p><b>Category:</b> <span>${
+                          result.media_type
+                        }</span></p>
+                      </div>
+                    </div>
+                  </div>`;
       });
-      document.querySelector(".container").innerHTML = output;
-      // console.log(data);
+      document.querySelector(".cards").innerHTML = output;
     })
     .catch((err) => console.log(err));
+  input.value = "";
 }
-
-fetchMovie();
